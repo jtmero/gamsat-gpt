ae70e79447f076505eec37864e60184e7ec45fe4
jtmero <86637429+jtmero@users.noreply.github.com>
2024-05-15 16:11:35 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,17 @@
 from openai import OpenAI
 import streamlit as st
+from dotenv import load_dotenv
 
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the API key from environment variable
+api_key = os.getenv('API_KEY')
+
+# Create the title
 st.title("GamsatGPT")
 
+# Load the model
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-4o"
 
