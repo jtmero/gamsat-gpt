cf235c616114a1158083af29e52231b026bec6e5
jtmero <86637429+jtmero@users.noreply.github.com>
2024-05-15 16:03:57 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,8 @@ import streamlit as st
 
 st.title("GamsatGPT")
 
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-4o"
+
 with st.chat_message("user"):
     st.write("Hello 👋")
