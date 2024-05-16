18cb5738b5919a8f964b6215669d6eccd1ab3d6f
jtmero <86637429+jtmero@users.noreply.github.com>
2024-05-16 11:42:56 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,15 @@
 from openai import OpenAI
 import streamlit as st
 
-st.title("ChatGPT-like clone")
-intro = "Hi! You can ask me to generate any kind of GAMSAT SIII question"
+# Set up the chat
+st.title("GamsatGPT")
+intro = "Welcome to GamsatGPT. You can ask me to generate any kind of GAMSAT SIII question"
 
+# Load the OpenAi client and assistant
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+assistant = openai_client.beta.assistants.retrieve("asst_3no7SQcpD6vOpUHqMCL2cRUB")
 
+# Initialize the session state
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-4o"
 
@@ -13,23 +17,30 @@ if "messages" not in st.session_state:
     st.session_state.messages = []
     st.session_state.messages.append({"role": "assistant", "content": intro})
 
+# Display the previous messages
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
+# Enter new messages to chat from the user
 if prompt := st.chat_input("Enter your reply"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
 
+    # Create a thread
+    client.beta.threads.create()
+    
+    # Append the prompt to the thread as a message
+    message = client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content=prompt)
+    
+    # Generate the assistant reply
     with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-        response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+        with client.beta.threads.runs.stream(
+            thread_id=thread.id,
+            assistant_id=assistant.id,
+        ) as stream:
+            stream.until_done()
