bd28f64cca5e41aab565e83aa06a3e86eb11b1de
jtmero <86637429+jtmero@users.noreply.github.com>
2024-06-07 21:27:30 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,43 +1,28 @@
 import streamlit as st
-import vertexai
-from vertexai.preview.language_models import TextGenerationModel
+from openai import OpenAI
+import re
 import os
 
 # Set up the chat
-st.title("GamsatGPT (Powered by Gemini)")
+st.title("GamsatGPT")
 st.subheader("Ask me to make you a question!")
 
-# Load the environment variables 
-project_id = os.environ["PROJECT_ID"]
-location = os.environ["LOCATION"]  
+# Load the environment variables
+api_key = os.environ["API_KEY"]
+asst_id = os.environ["ASST_ID"]
 
-# Initialize Vertex AI 
-vertexai.init(project=project_id, location=location)
-
-# Initialize the Gemini Pro 1.5 Flash Model with a system prompt.
-model = TextGenerationModel.from_pretrained("models/gemini-1.5-flash-001",
-    system_instruction="You are a helpful assistant that makes gamstat style questions")
-
-# Configuration for text generation
-generation_config = {
-    "max_output_tokens": 8192,
-    "temperature": 1,
-    "top_p": 0.95,
-}
-
-# Safety settings to filter out harmful content
-safety_settings = {
-    vertexai.preview.language_models.HarmCategory.HARM_CATEGORY_HATE_SPEECH: vertexai.preview.language_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    vertexai.preview.language_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: vertexai.preview.language_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    vertexai.preview.language_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: vertexai.preview.language_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    vertexai.preview.language_models.HarmCategory.HARM_CATEGORY_HARASSMENT: vertexai.preview.language_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-}
+# Load the client and retrieve the assistant
+client = OpenAI(api_key=api_key)
+assistant = client.beta.assistants.retrieve(asst_id)
 
 # Initialize session state
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-4o"
 if "messages" not in st.session_state:
     st.session_state.messages = []
 if "thread_id" not in st.session_state:
-    st.session_state.thread_id = 1 
+    thread = client.beta.threads.create()
+    st.session_state.thread_id = thread.id
 
 # Display messages
 for message in st.session_state.messages:
@@ -50,18 +35,34 @@ if prompt := st.chat_input("Enter your reply"):
     with st.chat_message("user"):
         st.markdown(prompt)
 
-    try:
-        # Generate and display the Gemini Pro reply
-        with st.spinner('Generating response...'):
-            responses = model.generate_text(
-                prompt=prompt,
-                generation_config=generation_config,
-                safety_settings=safety_settings,
-                stream=True,
-            )
+    # Append the prompt to the existing thread
+    client.beta.threads.messages.create(
+        thread_id=st.session_state.thread_id,
+        role="user",
+        content=prompt)
 
-            for response in responses:
-                with st.chat_message("assistant"):
-                    st.markdown(response.text, end="") 
-    except Exception as e:  
-        st.error(f"An error occurred: {e}")
+    # Generate and display the assistant reply
+    with st.spinner('Generating response...'):
+        stream = client.beta.threads.runs.create(
+            thread_id=st.session_state.thread_id,
+            assistant_id=assistant.id,
+            stream=True
+        )
+
+        # Loop through the streamed events to find the completed message
+        for event in stream:
+            if event.event == "thread.message.completed":
+                response = event.data.content
+
+                # Loop through the elements to find the text type response
+                for block in response:
+                    if block.type == 'text':
+                        # Extract the text from the `value` field
+                        text_content = block.text.value
+
+                        # Append this to session state
+                        st.session_state.messages.append({"role": "assistant", "content": text_content})
+                        
+                        # Display this text in Streamlit
+                        with st.chat_message("assistant"):
+                            st.markdown(text_content)
